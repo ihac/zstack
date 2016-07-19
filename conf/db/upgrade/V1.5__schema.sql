e9d0a96a4bf1485054fca13c12cce389dad3e52c
Mei Lei <meilei007@gmail.com>
2016-07-19 19:55:22 +0800

    Add cron support, finish update, query API and Unit Test
    Signed-off-by: Mei Lei <meilei007@gmail.com>

--- conf/db/upgrade/V1.5__schema.sql
+++ conf/db/upgrade/V1.5__schema.sql
@@ -9,8 +9,11 @@ CREATE TABLE  `zstack`.`ImageStoreBackupStorageVO` (
 
 CREATE TABLE  `zstack`.`SchedulerVO` (
     `uuid` varchar(32) NOT NULL UNIQUE,
-    `schedulerName` varchar(255),
-    `schedulerInterval` int unsigned NOT NULL,
+    `schedulerName` varchar(255) NOT NULL,
+    `schedulerType` varchar(255) NOT NULL,
+    `schedulerInterval` int unsigned DEFAULT 0,
+    `repeatCount` int unsigned DEFAULT 0,
+    `cronScheduler` varchar(255),
     `jobName` varchar(255),
     `jobGroup` varchar(255),
     `triggerName` varchar(255),
@@ -19,6 +22,8 @@ CREATE TABLE  `zstack`.`SchedulerVO` (
     `jobData` varchar(65535),
     `status` varchar(255),
     `createDate` timestamp DEFAULT CURRENT_TIMESTAMP,
-    `startDate` timestamp NOT NULL,
+    `startDate` timestamp,
+    `stopDate` timestamp,
+    `lastOpDate` timestamp,
     PRIMARY KEY  (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
